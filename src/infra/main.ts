34094700316d0d2d2b6e42e701d03bfecb1b5256
Filipe Siqueira <filipedev040990@gmail.com>
2024-06-24 16:52:21 -0300

    chore: add swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -4,6 +4,8 @@ import cors from 'cors'
 import { makeConsumeCreatedOrderQueueFactory } from './factories/tasks/consume-created-order-queue.factory'
 import { logger } from '@/shared/helpers/logger.helper'
 import { router } from './route'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '@/infra/swagger.json'
 
 const start = async (): Promise<void> => {
   try {
@@ -12,6 +14,7 @@ const start = async (): Promise<void> => {
     app.use(cors())
     app.use(express.json())
     app.use('/v1', router)
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
     const port = process.env.PORT ?? 3000
 
